de38c7fe0f2059b4b53afb39be4b4b376e5e2e36
Christine Augustine and Isaac Garnand <christine@>
2020-01-14 14:37:10 -0800

    added click functions and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,59 @@
 $(document).ready(function() {
-  $(".click").click(function() {
-    $(".hidden-element").toggle();
-})});
+  $(".click0").click(function() {
+    $(".hidden-element0").toggle();
+})  
+$(".click1").click(function() {
+  $(".hidden-element1").toggle();
+})
+$(".click2").click(function() {
+  $(".hidden-element2").toggle();
+})
+$(".click3").click(function() {
+  $(".hidden-element3").toggle();
+})
+$(".click4").click(function() {
+  $(".hidden-element4").toggle();
+})
+$(".click5").click(function() {
+  $(".hidden-element5").toggle();
+})
+$(".click6").click(function() {
+  $(".hidden-element6").toggle();
+})
+$(".click7").click(function() {
+  $(".hidden-element7").toggle();
+})
+$(".click8").click(function() {
+  $(".hidden-element8").toggle();
+})
+$(".click9").click(function() {
+  $(".hidden-element9").toggle();
+})
+$(".click10").click(function() {
+  $(".hidden-element10").toggle();
+})
+$(".click11").click(function() {
+  $(".hidden-element11").toggle();
+})
+$(".click12").click(function() {
+  $(".hidden-element12").toggle();
+})
+$(".click13").click(function() {
+  $(".hidden-element13").toggle();
+})
+$(".click14").click(function() {
+  $(".hidden-element14").toggle();
+})
+$(".click15").click(function() {
+  $(".hidden-element15").toggle();
+})
+$(".click16").click(function() {
+  $(".hidden-element16").toggle();
+})
+$(".click17").click(function() {
+  $(".hidden-element17").toggle();
+})
+$(".click18").click(function() {
+  $(".hidden-element18").toggle();
+})
+});
